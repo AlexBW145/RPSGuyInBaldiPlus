5a90c70e5d0303582ca5ec7bd5e3b79bc7594b78
AlexBW145 <92541723+AlexBW145@users.noreply.github.com>
2024-06-16 06:03:31 -0500

    Update GeneratorPatches.cs

--- RPSGuyInBaldiPlus/Patches/GeneratorPatches.cs
+++ RPSGuyInBaldiPlus/Patches/GeneratorPatches.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using MTM101BaldAPI;
 using System;
 using System.Linq;
@@ -12,39 +12,12 @@ namespace RPSGuyInBaldiPlus
     [HarmonyPatch("StartGenerate")]
     class GeneratorPatch
     {
-        private static WeightedItemObject nosquee;
-        private static bool gotSquee;
         static bool Prefix(LevelGenerator __instance)
         {
-            if (gotSquee)
-            {
-                nosquee = __instance.ld.items[5];
-                gotSquee = true;
-            }
-            __instance.ld.potentialNPCs.AddRange(RPSGuyInBaldiPlus.NpcList);
-            //IS NOT POSSIBLE WITH ARRAYS, THEY NEED TO CHANGE IT INTO LISTS.
-            if (ObjectFinders.GetFirstInstance((Character)13) != null)
-            {
-                __instance.ld.items[5] = RPSGuyInBaldiPlus.itemList.ToArray()[0];
-                __instance.ld.shopItems[5] = RPSGuyInBaldiPlus.itemList.ToArray()[0];
-            }
-            else
-            {
-                __instance.ld.items[5] = nosquee;
-                __instance.ld.shopItems[5] = nosquee;
-            }
+            __instance.ld.potentialNPCs.Add(RPSGuyInBaldiPlus.rpsGuy);
+            __instance.ld.items = __instance.ld.items.AddToArray(RPSGuyInBaldiPlus.HammerObject);
+            __instance.ld.shopItems = __instance.ld.shopItems.AddToArray(RPSGuyInBaldiPlus.HammerObject);
             return true;
         }
-
-        private void Awake()
-        {
-            RPSGuyInBaldiPlus.rpsGuy.GetComponent<Navigator>().ec = Singleton<BaseGameManager>.Instance.Ec;
-            RPSGuyInBaldiPlus.rpsGuy.GetComponent<RPSGuy>().ec = Singleton<BaseGameManager>.Instance.Ec;
-            RPSGuyInBaldiPlus.rpsGuy.GetComponent<Navigator>()._startTile = Singleton<EnvironmentController>.Instance.npcSpawnTile[0];
-            RPSGuyInBaldiPlus.rpsGuy.GetComponent<Navigator>()._targetTile = Singleton<EnvironmentController>.Instance.npcSpawnTile[0];
-        }
-
-        // FOR STUPIDS
-        // idk how to make npcs spawn in out of floor 1..
     }
 }
